a142212492abc22b5f034481e81a4d05155c0803
Sergey Mihaylov <s.mihaylov@fbs.com>
2019-01-02 18:27:47 +0300

    Add random media type selection according to media count

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM golang:alpine as build
+FROM golang:alpine3.8 as build
 COPY . /go/src/github.com/chesnovsky/fliboobstier
 WORKDIR /go/src/github.com/chesnovsky/fliboobstier
 RUN  apk add --no-cache git make gcc libc-dev ca-certificates \
   && make deps \
   && make
 
-FROM library/alpine
+FROM library/alpine:3.8
 RUN apk add --no-cache ca-certificates
 COPY config.yml /config.yml
 COPY --from=build /go/src/github.com/chesnovsky/fliboobstier/bin /fliboobstier
--- bot/bot.go
+++ bot/bot.go
@@ -4,10 +4,19 @@ import (
     "github.com/chesnovsky/fliboobstier/config"
     "github.com/chesnovsky/fliboobstier/logger"
     "gopkg.in/telegram-bot-api.v4"
+    "math/rand"
 )
+
+type MediaTypesType struct{
+    Wtf int
+    Image int
+    Sticker int
+    Gif int
+}
 
 var (
     TgBot = tgbotapi.BotAPI{}
+    MediaTypes = MediaTypesType{-1, 0, 1, 2}
 )
 
 // func initBotApi(token string) error {
@@ -61,9 +70,68 @@ func sender(bot *tgbotapi.BotAPI, message *tgbotapi.Message, senderConfig config
     }
 }
 
+func isNumInRange(rMin int, rMax int, num int) bool{
+    if (rMin < num && num < rMax) {
+        return true
+    } else {
+        return false
+    }
+}
+
+func selectMediaType(config config.WordCatch) int {
+    imagesLen   := len(config.Images)
+    gifsLen     := len(config.Gifs)
+    stickersLen := len(config.Stickers)
+    totalLen    := (imagesLen + gifsLen + stickersLen + 1)
+    logger.Logger.Debugf("Images <%d> Stickers <%d> Gifs <%d>", imagesLen, stickersLen, gifsLen)
+
+    typeIndex := rand.Intn(totalLen)
+    logger.Logger.Debugf("Rolled type index <%d>", typeIndex)
+
+    if isNumInRange(-1, imagesLen + 1, typeIndex) && imagesLen != 0 {
+        return MediaTypes.Image
+    } else if isNumInRange(imagesLen , imagesLen + gifsLen + 1, typeIndex) {
+        return MediaTypes.Gif
+    } else if isNumInRange(imagesLen + gifsLen, imagesLen + gifsLen + stickersLen + 1, typeIndex) {
+        return MediaTypes.Sticker
+    } else {
+        return MediaTypes.Wtf
+    }
+}
+
+
 func createSenderMsg(senderConfig config.WordCatch, chatID int64) (tgbotapi.Chattable, error) {
-    // select media type
-    var err error
-    msg := tgbotapi.NewStickerShare(chatID, senderConfig.Stickers[0])
+    var(
+        err error
+        media string
+        msg tgbotapi.Chattable
+    )
+    mediaType := selectMediaType(senderConfig)
+    switch mediaType {
+    case MediaTypes.Image:
+        mediaLen := len(senderConfig.Images)
+        media = senderConfig.Images[rand.Intn(mediaLen)]
+        msg = tgbotapi.NewPhotoShare(chatID, media)
+    case MediaTypes.Sticker:
+        mediaLen := len(senderConfig.Stickers)
+        media = senderConfig.Stickers[rand.Intn(mediaLen)]
+        msg = tgbotapi.NewStickerShare(chatID, media)
+    case MediaTypes.Gif:
+        mediaLen := len(senderConfig.Gifs)
+        media = senderConfig.Gifs[rand.Intn(mediaLen)]
+        msg = tgbotapi.NewAnimationShare(chatID, media)
+    }
+    logger.Logger.Debugf("Got random media type<%d>:\t%s", mediaType, media)
     return msg, err
 }
+
+
+
+
+
+
+
+
+
+
+
